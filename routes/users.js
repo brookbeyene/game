92a4d6e79327a2d4c4ae243f23639591db4f83ab
brookbeyene <brookbeyene37@gmail.com>
2022-01-04 23:48:52 -0700

    Added the filter for the country listings

--- routes/users.js
+++ routes/users.js
@@ -2,34 +2,46 @@ var express = require('express');
 var router = express.Router();
 const AWS = require('aws-sdk');
 var countries = require('../country.json');
-var total = 0;
 
+var total = 0;
 var myData = []
+
 require('dotenv/config')
-/* GET users listing. */
+/* AWS Config Env. */
 AWS.config.update({
   region: process.env.AWS_DEFAULT_REGION,
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 });
 
+// AWS
 const dynamoClient = new AWS.DynamoDB.DocumentClient();
 const TABLE_NAME = "talent";
-router.get('/', function(req, res, next) {
-  // getCountries().then(response =>{
-  //   res.json(response.Item)
-  // })
-  // res.render('users', {username: TABLE_NAME, countryName: "", countryCode: "", possibleScore: "", total: 0})
-    res.render('users', {username: TABLE_NAME, 'myData': myData});
 
-  
+
+router.get('/', function(req, res, next) {
+    res.render('users', {username: TABLE_NAME, 'myData': myData});
 });
 
+const pitstop1 = (req, res, next) =>{
+  console.log("this is pit stop", req.body.countryName);
+  if(countries.ref_country_codes.filter(result => result.country === req.body.countryName)){
+    const result = countries.ref_country_codes.filter(result => {
+      return result.country.includes(req.body.countryName)
+    })
+    console.log("Filtered list", result)
+    next();
+  }else{
+    total= 0;
+    next();
+  }
+}
 
-router.post('/', function(req, res, next){
+router.post('/', pitstop1, function(req, res, next){
   const countryName = req.body.countryName;
   const countryCode = req.body.countryCode;
   const possible_score = (countryName.length) + (countryCode.length)
+
   total += possible_score;
   var dataset={
     "id": Date.now().toString(),
